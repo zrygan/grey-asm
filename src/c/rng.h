988f09acf0c567f6a11874f05f456ea7e4770532
Zhean Ganituen <zr.gntn@gmail.com>
2025-11-12 14:21:30 +0700

    Added documentation

--- src/c/rng.h
+++ src/c/rng.h
@@ -3,11 +3,20 @@
 
 #include <stdlib.h>
 
+/**
+ * @brief Generates a random integer within the range of [0,255]
+ * 
+ */
 inline unsigned int randint()
 {
     return (unsigned int) ((rand() % 255) + 1 );
 }
 
+/**
+ * @brief Generates a random double-precision floating point number within the range of
+ * [0.00000, 0.99999]
+ * 
+ */
 inline double randdbl()
 {
     return (rand() % 99999 + 1) * 0.00001;
