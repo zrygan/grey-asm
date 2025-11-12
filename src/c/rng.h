cbabe3dbef6490736fed9f840e62a3d24355821c
Zhean Ganituen <zr.gntn@gmail.com>
2025-11-12 13:57:20 +0700

    Random number generation

---
+++ src/c/rng.h
@@ -0,0 +1,17 @@
+#ifndef RNG_H
+#define RNG_H
+
+#include <time.h>
+#include <stdlib.h>
+
+inline unsigned int randint()
+{
+    return (unsigned int) ((rand() % 255) + 1 );
+}
+
+inline double randdbl()
+{
+    return (rand() % 99999 + 1) * 0.00001;
+}
+
+#endif // RNG_H
