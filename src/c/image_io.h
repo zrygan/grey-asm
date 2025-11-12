246f7d4eb130e6b1a4c7cbcf213502cc848f30c7
Zhean Ganituen <zr.gntn@gmail.com>
2025-11-12 13:57:14 +0700

    Added input and output functions for the image structs

---
+++ src/c/image_io.h
@@ -0,0 +1,80 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include "image.h"
+#include <string.h>
+
+/**
+ * @brief Displays the IntGrey to the standard output
+ */
+inline void displayIG(IntGrey img)
+{
+    for (unsigned int i = 0; i < img.height; i++)
+    {
+        for (unsigned int j = 0; j < img.width; j++)
+        {
+            printf("%3d ", img.matrix[i][j]);
+        }
+
+        printf("\n");
+    }
+}
+
+/**
+ * @brief Populates the IntGrey from user input
+ * 
+ * @note I don't think we need to use this, for testing purposes only.
+ */
+inline void inputIG(IntGrey img, char *message)
+{
+    const char *msg = (message == NULL) ? "" : message;
+
+    for (unsigned int i = 0; i < img.height; i++)
+    {
+        for (unsigned int j = 0; j < img.width; j++)
+        {
+            printf("%s", msg);
+            scanf("%d", &img.matrix[i][j]);
+        }
+    }
+}
+
+/**
+ * @brief Displays the IntGrey to the standard output
+ */
+inline void displayDG(DblGrey img)
+{
+    {
+        for (unsigned int i = 0; i < img.height; i++)
+        {
+            for (unsigned int j = 0; j < img.width; j++)
+            {
+                printf("%3.2lf ", img.matrix[i][j]);
+            }
+
+            printf("\n");
+        }
+    }
+}
+
+/**
+ * @brief Populates the DblGrey from user input
+ * 
+ * @note I don't think we need to use this, for testing purposes only.
+ */
+inline void inputDG(DblGrey img, char *message)
+{
+    const char *msg = (message == NULL) ? "" : message;
+
+    for (unsigned int i = 0; i < img.height; i++)
+    {
+        for (unsigned int j = 0; j < img.width; j++)
+        {
+            printf("%s", msg);
+            scanf("%lf", &img.matrix[i][j]);
+        }
+    }
+}
+
+
+#endif // UTIL_H
