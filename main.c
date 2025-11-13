1c032e7e762f42ab261ff7809aa0756d93c15729
Zhean Ganituen <zr.gntn@gmail.com>
2025-11-13 16:41:24 +0700

    Update main file with boiler plate code

--- main.c
+++ main.c
@@ -3,9 +3,24 @@
 #include "src/c/image.h"
 #include "src/c/image_io.h"
 #include "src/c/rng.h"
+#include "src/c/perf.h"
+
+void printDetails()
+{
+    printf("===== DETAILS =====\n");
+    printf("time: %10.8lf\n", diff);
+}
 
 int main()
 {
     srand(time(NULL));
+
+    startTime(&stime, &freq);
+
+    /* ADD code here*/
+
+    endTime(&stime, &etime, &freq, &diff);
+    printDetails();    
+    
     return 0;
 }
