159b18f39c7ef55f1e24843121043a566295fe23
Zhean Ganituen <zr.gntn@gmail.com>
2025-11-20 20:12:07 +0800

    Added Visual Studio solution

--- tests/run_asm_test.py
+++ tests/run_asm_test.py
@@ -5,7 +5,8 @@ import os
 # Change to script directory
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-subprocess.run(["nasm", "-f", "win64", "../src/asm/converter.asm", "-o", "converter.obj"])
+# nasm -f win64 converter.asm -o converter.obj
+subprocess.run(["nasm", "-f", "win64", "    ", "-o", "converter.obj"])
 
 subprocess.run(["gcc", "-I", "../src/c", "asm_test.c", "converter.obj", "-o", "asm_test.exe"])
 result = subprocess.run(["./asm_test.exe"])
---
+++ vs/vs/test.c
@@ -0,0 +1,79 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <stdint.h>
+//#include <time.h>
+//
+////
+//// Assembly function
+////
+//extern void imgCvtGrayDoubleToInt(const double* src, uint8_t* dst, size_t n);
+//
+////
+//// Generate random image (double 0.0 to 1.0)
+////
+//void randomizeDoubleImage(double* img, size_t n)
+//{
+//    for (size_t i = 0; i < n; i++)
+//        img[i] = ((int)(((double)rand() / RAND_MAX) * 100)) / 100.0;   // 0.0–1.0
+//}
+//
+////
+//// Print double matrix
+////
+//void printDoubleMatrix(double* img, int rows, int cols)
+//{
+//    printf("DOUBLE IMAGE:\n");
+//    for (int r = 0; r < rows; r++) {
+//        for (int c = 0; c < cols; c++) {
+//            printf("%0.2f ", img[r * cols + c]);
+//        }
+//        printf("\n");
+//    }
+//}
+//
+////
+//// Print integer matrix (0–255)
+////
+//void printIntMatrix(uint8_t* img, int rows, int cols)
+//{
+//    printf("INT IMAGE (0-255):\n");
+//    for (int r = 0; r < rows; r++) {
+//        for (int c = 0; c < cols; c++) {
+//            printf("%3d ", img[r * cols + c]);
+//        }
+//        printf("\n");
+//    }
+//}
+//
+//int main()
+//{
+//    srand((unsigned)time(NULL));
+//
+//    // Change these if you want
+//    int rows = 3;
+//    int cols = 4;
+//    size_t n = rows * cols;
+//
+//    printf("%d x %d\n", rows, cols);
+//
+//    // Allocate
+//    double* src = malloc(sizeof(double) * n);
+//    uint8_t* dst = malloc(sizeof(uint8_t) * n);
+//
+//    // Random fill image
+//    randomizeDoubleImage(src, n);
+//
+//    // Print source image
+//    printDoubleMatrix(src, rows, cols);
+//
+//    // Convert using your ASM function
+//    imgCvtGrayDoubleToInt(src, dst, n);
+//
+//    // Print output image
+//    printIntMatrix(dst, rows, cols);
+//
+//    free(src);
+//    free(dst);
+//
+//    return 0;
+//}
